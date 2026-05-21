f436589a1c8d90904100ffdea0942a13a88b335e
Uday Nand <udayweb3@gmail.com>
2026-05-21 23:39:31 +0530

    added changes to generate environment.ts file on realtime building

--- setup-env.js
+++ setup-env.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-
+fs.mkdirSync('./src/environments', { recursive: true });
 const content = `export const environment = {
   production: true,
   geminiApiKey: '${process.env.GEMINI_API_KEY}'
