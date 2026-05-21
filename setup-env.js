194d63047da9aca188f9caf9d0e0eae42cfa8791
Uday Nand <udayweb3@gmail.com>
2026-05-21 23:36:56 +0530

    Create setup-env.js to generate environment.ts

---
+++ setup-env.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const content = `export const environment = {
+  production: true,
+  geminiApiKey: '${process.env.GEMINI_API_KEY}'
+};
+`;
+
+fs.writeFileSync('./src/environments/environment.ts', content);
+console.log('✅ environment.ts generated successfully');
